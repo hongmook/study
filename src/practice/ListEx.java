289c2eb4949a0541de86e6f56a23d65746d60951
hongmook <ghdanr6589@naver.com>
2022-07-18 14:44:14 +0900

    프레임 워크

---
+++ src/practice/ListEx.java
@@ -0,0 +1,29 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListEx {
+
+	public static void main(String[] args) {
+		List list = new ArrayList();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(6);
+		
+		System.out.println(list);
+		//list 인터페이스는 legnth가 안됨
+		for(int i=0; i<list.size(); i++) {
+			System.out.println(i+":"+list.get(i));
+		}
+		
+		
+		
+		
+
+	}
+
+}
